1b4d07da45db95f96b5b58ad559f4b6006056fb3
Lily Peterson <lilypeterson444@gmail.com>
2025-03-06 17:49:15 -0500

    Reorganized Codebase into Functions and Starting to fix curriculum

--- Pygame Project.py
+++ Pygame Project.py
@@ -2,24 +2,15 @@ import pygame
 import pygame.freetype #Used for Text
 print("Pygame Version: ", pygame.ver)
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Infinite Dungeons")
-GAME_FONT = pygame.freetype.Font("freesansbold.ttf", 24)
-running = True
 
-clock = pygame.time.Clock()
-player = pygame.Rect(400, 300, 50, 50)
-
-#Run Window Till Closed
-while running:
-    #Setup Window
+def CreatePlayer(screen, player, clock):
     screen.fill((153, 50, 168))
     pygame.draw.rect(screen, (0, 255, 0), player)
-    pygame.display.flip()
+    #pygame.display.flip()
     clock.tick(60)
-    #Controls
-    keys = pygame.key.get_pressed()
+    
+    
+def Controls(player, keys, grid):
     if keys[pygame.K_UP]:
         player.y -= 5
     if keys[pygame.K_DOWN]:
@@ -36,18 +27,57 @@ while running:
         player.x = 750
     if player.y > 550:
         player.y = 550
-    #Post Text to Screen
-    GAME_FONT.render_to(screen, (40, 350), "Hello World!", (0, 0, 0))
-    pygame.display.flip()
-    #Tile System
-    grid = [[1 if ( x + y) % 2 == 0 else 0 for x in range(10)] for y in range(10)]
+    #New Controls Implementation
+    '''
+    new_x = player.x // 60
+    new_y = player.y // 60
+    if grid[new_y][new_x] == 1:
+        player.x = new_x * 60
+        player.y = new_y * 60
+    '''
+        
+def GameText(text, screen, position, font):
+    font.render_to(screen, position, text, (0, 0, 0))
+    #pygame.display.flip()
+
+def GeneratePerlinNoise(grid, screen):
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             color = (200, 200, 200) if grid[y][x] == 1 else (50,50,50)
             pygame.draw.rect(screen, color, pygame.Rect(x*60, y*60, 60, 60))
-    #Close Window on Exit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-pygame.quit()
+#def Quit():
+    
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Infinite Dungeons")
+    GAME_FONT = pygame.freetype.Font("NotoSansWarangCiti-Regular.ttf", 24)
+    clock = pygame.time.Clock()
+    player = pygame.Rect(400, 300, 50, 50)
+    running = True
+
+
+    #Run Window Till Closed
+    while running:
+        #Setup Window
+        CreatePlayer(screen, player, clock)
+        #Controls
+        keys = pygame.key.get_pressed()
+        grid = [[1 if ( x + y) % 2 == 0 else 0 for x in range(10)] for y in range(10)]
+        Controls(player, keys, grid)
+        #Post Text to Screen
+        GameText("Hello World!", screen, (40, 350), GAME_FONT)
+        #Tile System
+        GeneratePerlinNoise(grid, screen)
+        pygame.display.update()
+        #Close Window on Exit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
